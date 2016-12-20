920922f2953f6f91996408eb110dd9296949f05f
wang-liang <wang.liang@navercorp.com>
2016-12-20 14:21:15 +0800

    delete

--- src/main/java/com/shanks/learn/ui/BootApplication.java
+++ src/main/java/com/shanks/learn/ui/BootApplication.java
@@ -3,13 +3,22 @@ package com.shanks.learn.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BootApplication {
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate template() {
+		return new RestTemplate();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 	}
-	
+
 }
